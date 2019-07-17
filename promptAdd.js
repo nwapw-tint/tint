21903d425f3efdb2eb42b1c8ba60658d8c4c57b2
Eric Xia <ericpingxia@gmail.com>
2019-07-17 13:26:56 -0700

    added timestamp/text

--- promptAdd.js
+++ promptAdd.js
@@ -1,30 +1,42 @@
-/*specArray = prompt('input duration, name, opacity, and color').split(',');
-var thisName = specArray[1];
-addTimedFilter(specArray[0],thisName,specArray[2],specArray[3]);
-//adds a filter to the screen for the specified duration. Only works on current page.
-function addTimedFilter(duration,name, opacity, color)
-{ */
-//parameter definition
 name = "tint-"+"salmon"
-color = "#fa8072"
-opacity = 0.3;
+color = "#000"
+opacity = 0.6;
 duration = 100;
+text = "English HW 3:27";
+
 if(document.querySelector('[id^="tint-"]') == null)
 {
     console.log("no existing filter found, adding another");
+    var body = document.body;
     var tintDiv = document.createElement("div");
+    var textDiv = document.createElement("div");
     tintDiv.id = name; //allows removal by id
     tintDiv.style.opacity = opacity;
     tintDiv.style.background = color;
-    makeTint(tintDiv);
-    document.body.appendChild(tintDiv);
-
+    styleTint(tintDiv);
+    styleText(text);
+    body.style.height = "100%";
+    body.style.margin = 0;
+    body.style.padding = 0;
+    body.appendChild(tintDiv);
+    tintDiv.appendChild(textDiv);
+    textDiv.appendChild(document.createTextNode(text));
     setTimeout(function (){
         var overlay = document.getElementById(name); //removes the specified filter by id
         overlay.parentNode.removeChild(overlay);
         },duration*1000);
+
 }else{console.log("An existing filter was found.")}
-function makeTint(div) {
+function styleText(text)
+{
+    textDiv.style.position = "absolute";
+    textDiv.style.left  = "50%";
+    textDiv.style.top = "50%";
+    textDiv.style.backgroundColor = "white";
+    textDiv.style.zIndex = 100;
+
+}
+function styleTint(div) {
     div.style.width = "100%";
     div.style.height = "100%";
     div.style.pointerEvents = "none";
@@ -33,4 +45,3 @@ function makeTint(div) {
     div.style.left = 0;
     div.style.position = "fixed";
 }
-//}
