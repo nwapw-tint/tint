251bf9a914504351475e5634507bbdec2d376fc6
Eric Xia <ericpingxia@gmail.com>
2019-07-18 08:43:24 -0700

    Update background.js
    Removed extraneous commenting

--- background.js
+++ background.js
@@ -1,22 +1,3 @@
-/*
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-chrome.tabs.executeScript({
-file: 'promptAdd.js'
-})
-})
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    chrome.tabs.executeScript({
-        file: 'promptAdd.js'
-        })
-});
-
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    chrome.tabs.executeScript({
-        file: 'promptAdd.js'
-        })
-  })
-  */
 //experimental 
 chrome.omnibox.onInputEntered.addListener(
     function(txt) {
