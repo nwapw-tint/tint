5f979ed64af07a0739856d896129af282b612c5c
Eric Xia <ericpingxia@gmail.com>
2019-07-17 18:25:48 -0700

    declarative instead of programmatic injection
    Changed the activation of the script to declarative. It will now run at "document_start" for almost all sites (some still don't work, i'm not sure why)

--- promptAdd.js
+++ promptAdd.js
@@ -1,25 +1,28 @@
-name = "tint-"+"salmon"
-color = "#000"
-opacity = 0.6;
+name = "tint-"+"colorName"
+color = "rgba(0,0,0,0.8)";
 duration = 100;
 text = "English HW 3:27";
 
 if(document.querySelector('[id^="tint-"]') == null)
 {
     console.log("no existing filter found, adding another");
-    var body = document.body;
+    var doc = document.documentElement;//running at document_start means there is no head or body
     var tintDiv = document.createElement("div");
     var textDiv = document.createElement("div");
+    var robotoFont = document.createElement('link');
+    robotoFont.setAttribute('rel', 'stylesheet');
+    robotoFont.setAttribute('type', 'text/css');
+    robotoFont.setAttribute('href', "https://fonts.googleapis.com/css?family=Roboto&display=swap");
+    doc.appendChild(robotoFont); 
     tintDiv.id = name; //allows removal by id
-    tintDiv.style.opacity = opacity;
     tintDiv.style.background = color;
+    doc.style.height = "100%";
+    doc.style.margin = 0;
+    doc.style.padding = 0;
+    doc.appendChild(tintDiv);
+    tintDiv.appendChild(textDiv);
     styleTint(tintDiv);
     styleText(text);
-    body.style.height = "100%";
-    body.style.margin = 0;
-    body.style.padding = 0;
-    body.appendChild(tintDiv);
-    tintDiv.appendChild(textDiv);
     setTimeout(function (){
         var overlay = document.getElementById(name); //removes the specified filter by id
         overlay.parentNode.removeChild(overlay);
@@ -33,9 +36,10 @@ function styleText(text)
     textDiv.style.left = "50%";
     textDiv.style.marginRight = "-50%";
     textDiv.style.transform="translate(-50%, -50%)";
-    textDiv.style.backgroundColor = "black";
-    textDiv.style.color = "#ff69b4" //TODO: set automatically based on tint shade
-    textDiv.style.fontSize = "240px" //TODO: self adjusting size. rn, just set a cap
+    textDiv.style.backgroundColor = "rgba(255,255,255,1)";
+    textDiv.style.fontFamily = "'Roboto', Sans Serif";
+    textDiv.style.color = "#000"; //TODO: set automatically based on tint shade
+    textDiv.style.fontSize = "60px"; //TODO: self adjusting size. rn, just set a cap
     textDiv.style.zIndex = 100;
     textDiv.innerHTML = "read pg. 64, 65";
 }
